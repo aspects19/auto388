8102637cd3a78cd2efccfc2e0e7904dfa937b9a5
aspect <jeffamenya388@gmail.com>
2024-09-05 00:51:26 +0300

    Added Sqlite as the chats database

--- index.js
+++ index.js
@@ -1,167 +1,157 @@
-const {makeWASocket, DisconnectReason, useMultiFileAuthState, fetchLatestBaileysVersion, MessageType, MessageOptions, Mimetype } = require ("@whiskeysockets/baileys");
-const pino = require("pino");    
-const fs = require('fs');
+const {
+  default: AMDConnect,
+  useMultiFileAuthState,
+  DisconnectReason,
+  downloadMediaMessage,
+  
+} = require("@whiskeysockets/baileys");
+const pino = require("pino");
+const fs = require("fs");
+const Database = require('better-sqlite3');
 
-function appendToJson(file_path, key, value) {
-  try {
-    const data = JSON.parse(fs.readFileSync(file_path));
-    data[key] = value;
-    fs.writeFileSync(file_path, JSON.stringify(data, null, 2));
-  } catch (error) {
-    fs.writeFileSync(file_path, JSON.stringify({ [key]: value }, null, 2));
-  }
-};
-function getKeysArrayFromJson(file_path)  {
-try {
-  const data = JSON.parse(fs.readFileSync(file_path));
-  const keys = Object.keys(data);
-  return keys;
-  } catch (error) {
-    console.error(`Error reading file: ${file_path}`);
-    return [];
-  }
-};
-function isKeyValueMatch(filePath, key, value) {
-  try {
-    const jsonString = fs.readFileSync(filePath, 'utf-8');
-    const jsonObject = JSON.parse(jsonString);
-    if (jsonObject.hasOwnProperty(key)) {
-      return jsonObject[key] === value && jsonObject[key] !== "false";
-    } else {
-      return false;
-    }
-  } catch (error) {
-    console.error('Error reading or parsing the JSON file:', error.message);
-    return false;
-  }
-};
+const owner = "254794141227@s.whatsapp.net";
 
-function getValueOfkey(file_path, key) {
-  try {
-      const jsonData = JSON.parse(fs.readFileSync(file_path, 'utf-8'));
-      return jsonData[key] !== undefined ? jsonData[key] : `Key '${key}' not found in the JSON file.`;
-  } catch (error) {
-      if (error.code === 'ENOENT') {
-          return `File not found: ${file_path}`;
-      } else {
-          return `Error reading or parsing JSON file: ${file_path}`;
-      }
-  }
-};
+const db = new Database('whatsapp.db');
+db.prepare(`
+  CREATE TABLE IF NOT EXISTS messages (
+    id TEXT PRIMARY KEY,
+    remoteJid TEXT,
+    pushName TEXT,
+    conversation TEXT,
+    timestamp INTEGER
+  )
+`).run();
+
+async function startAMD() {
+  console.log('Script Online...');
+  const { state, saveCreds } = await useMultiFileAuthState("./session");
+  const AMD = AMDConnect({
+    logger: pino({ level: "silent" }),
+    printQRInTerminal: true,
+    browser: ["Ubuntu", "Chrome", "20.0.04"],
+    auth: state,
+    markOnlineOnConnect: false,
+  });
 
 
-  async function statusAutoView() {
-
-    const { state, saveCreds } = await useMultiFileAuthState("./SESSION");
-    const { version } = await fetchLatestBaileysVersion();
-
-    const bot = makeWASocket({
-        logger: pino({ level: "silent" }),
-        version,
-        printQRInTerminal: true,
-        browser: ["RENDER", "Safari", "3.O"],
-        auth: state,
-        markOnlineOnConnect : false
-      });
-
-    bot.ev.on("connection.update", async (update) => {
-      const { connection, lastDisconnect } = update;
-      if (connection === "close") {
-        var a, b;
-        var shouldReconnect =
-          ((b =
-            (a = lastDisconnect.error) === null || a === void 0
-              ? void 0
-              : a.output) === null || b === void 0
-            ? void 0
-            : b.statusCode) !== DisconnectReason.loggedOut;
-        console.log(
-          "connection closed due to ",
-          lastDisconnect.error,
-          ", reconnecting ",
-          shouldReconnect
-        );
-        if (shouldReconnect) {
-          statusAutoView();
-        }
-      } else if (connection === "open") {
-        console.log("Logged in successfully");
-      }
-    });
-    
-    bot.ev.on("creds.update", saveCreds);
-    bot.ev.on("messages.upsert", async (m) => {
-      m.messages.forEach(async (message) => {
-        if (!message.message || message.message.ephemeralMessage)
-          return;
-        //view on
-        if (message.key && message.key.remoteJid == "status@broadcast") {
-          setTimeout(async () => {
-            try {
-              const ignoreData = fs.readFileSync("ignoreList.json",'utf-8')
-              const ignoreObject = await JSON.parse(ignoreData);
-              if (message.key.participant in ignoreObject) {
-                console.log(`${message.pushName} in ignore list`);
-                } else{
-                await bot.readMessages([message.key]);
-              console.log( (message.message.protocolMessage ? `\u2757${message.pushName} deleted their story` : `Viewed ${message.pushName}'s stories`));
-              
-              }
-            } catch (err) {
-              console.error("Error reading messages:", err);
-            }
-          }, 59000);
-          appendToJson("./contactList.json", message.key.participant, message.pushName)
-        };
+  AMD.ev.on("messages.upsert", async (chatUpdate) => {
+    try {
+      const m = chatUpdate.messages[0];
       
-        //add number to ignore list
-        if (message.key.fromMe && message.message.conversation.startsWith(".ignore") ) {
-          try {
-            const ignoreNumber = (message.message.conversation.split(" ")[1]+"@s.whatsapp.net");
-            try {
-              appendToJson("./ignoreList.json",ignoreNumber, getValueOfkey("./contactList.json",ignoreNumber));
-            } catch (err) {
-              appendToJson("./ignoreList.json",ignoreNumber,"-");
-            }
-          } catch (err) {
-            console.error("Error reading messages:", err);
-          }
-        };
+      if (!m) return;
+      if (m.key.remoteJid=="status@broadcast") return;
+      if (m.key.remoteJid.endsWith("g.us")) return;
 
-        //send the contact list file to me
-        if (message.key.fromMe && message.message.conversation === ".clist" ) {
-          try {
-            await bot.sendMessage("254794141227@s.whatsapp.net" ,{document: fs.readFileSync('./contactList.json'), Mimetype: "application/json", fileName : "contactList.json", }) 
-          } catch (err) {
-           console.log(err);
-          }
-          
-        };
-
-        //remove a contact from ignore list
-        if (message.key.fromMe && message.message.conversation.startsWith(".del")) {
-          try {
-            console.log(`Trying delete`);
-            const deleteNumber = (message.message.conversation.split(" ")[1]+"@s.whatsapp.net");
-            const deleteData = fs.readFileSync("ignoreList.json",'utf-8');
-            const deleteObject = JSON.parse(deleteData);
-            try {
-            
-              delete deleteObject[deleteNumber]
-              fs.writeFileSync('./ignoreList.json',JSON.stringify(deleteData, null))
-            } catch (err) {
-              console.log(`Key ${deleteNumber} not found`);
-            }
-          } catch (err) {
-            console.log(err);
-          }
-        };
-
+      if (m.message.viewOnceMessageV2) {
         
-      });
-    });
-  };
+        const buffer = await downloadMediaMessage (
+          m, 
+          'buffer', {}, {
+            logger: pino(),
+            reuploadRequest: AMD.updateMediaMessage
+          }
+        )
+          
+        await AMD.sendMessage(
+          owner, 
+            (m.message.viewOnceMessageV2.message.videoMessage) ? {video: buffer}: {image: buffer}
+          
+        )
+        
+      };
 
+      if (m.key.fromMe && m.message?.extendedTextMessage?.text == ".db" ) {
+        try {
+          await AMD.sendMessage(owner ,{document: fs.readFileSync('./whatsapp.db'), Mimetype: "application/x-sqlite3", fileName : "whatsapp.db", }) 
+        } catch (err) {
+         console.log(err);
+        }
+        
+      };
 
-statusAutoView();
+      if (m.key.fromMe && m.message?.extendedTextMessage?.text?.startsWith(".dp")) {
+        const jid = m.message.extendedTextMessage.text.split(" ")[1]+"@s.whatsapp.net";
+        
+        const url = await AMD.profilePictureUrl(jid, 'image');
+        await AMD.sendMessage(owner, { 
+          image: { url: url }, 
+          mimetype: "image/jpeg",
+        });
+        
+      };
 
+      if (m.key.remoteJid.endsWith("@s.whatsapp.net") && m.message.conversation !=='' && !m.key.fromMe) {
+        const messageData = {
+          remoteJid: m.key.remoteJid,
+          id: m.key.id,
+          pushName: m.pushName,
+          conversation: m.message?.conversation || '',
+          timestamp: m.messageTimestamp, 
+        };
+
+        const insert = db.prepare(`
+          INSERT INTO messages (id, remoteJid, pushName, conversation, timestamp)
+          VALUES (?, ?, ?, ?, ?)
+        `);
+        insert.run(messageData.id, messageData.remoteJid, messageData.pushName, messageData.conversation, messageData.timestamp);
+      }
+
+      if (m.message.protocolMessage) {
+        const deletedId = m.message.protocolMessage.key.id;
+        const deletedMessage = db.prepare(`
+          SELECT * FROM messages WHERE id = ?
+        `).get(deletedId);
+
+        if (deletedMessage) {
+          AMD.sendMessage(owner,{text :`*${deletedMessage.pushName} deleted* \n${deletedMessage.conversation}` })
+        } else {
+          console.log('No message found with ID:', deletedId);
+        }
+      }
+      
+    } catch (err) {
+      console.log('Error:', err);
+    }
+  });
+
+  AMD.ev.on("connection.update", async (update) => {
+    const { connection, lastDisconnect } = update;
+    if (connection === "close") {
+      let reason = lastDisconnect.error
+        ? lastDisconnect?.error?.output.statusCode
+        : 0;
+      if (reason === DisconnectReason.badSession) {
+        console.log(`Bad Session File, Please Delete Session and Scan Again`);
+        process.exit();
+      } else if (reason === DisconnectReason.connectionClosed) {
+        console.log("Connection closed, reconnecting....");
+        startAMD();
+      } else if (reason === DisconnectReason.connectionLost) {
+        console.log("Connection Lost from Server, reconnecting...");
+        startAMD();
+      } else if (reason === DisconnectReason.connectionReplaced) {
+        console.log(
+          "Connection Replaced, Another New Session Opened, Please Close Current Session First"
+        );
+        process.exit();
+      } else if (reason === DisconnectReason.loggedOut) {
+        console.log(`Device Logged Out, Please Delete Session and Scan Again.`);
+        process.exit();
+      } else if (reason === DisconnectReason.restartRequired) {
+        console.log("Restart Required, Restarting...");
+        startAMD();
+      } else if (reason === DisconnectReason.timedOut) {
+        console.log("Connection TimedOut, Reconnecting...");
+        startAMD();
+      } else {
+        console.log(`Unknown DisconnectReason: ${reason}|${connection}`);
+      }
+    }
+  });
+
+  AMD.ev.on("creds.update", saveCreds);
+
+  return AMD;
+};
+
+startAMD();
---
+++ test.js
@@ -0,0 +1,4 @@
+const text = "first second"
+
+const desr = text.split(" ")[1]
+console.log(desr)
